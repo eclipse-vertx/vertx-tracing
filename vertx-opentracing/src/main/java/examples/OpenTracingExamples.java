cd49f41791d8c942a53bbe74109be121cde8cc80
Joel Takvorian <joel.takvorian@qaraywa.net>
2020-09-23 15:40:45 +0200

    Add opentracing example to set span
    Signed-off-by: Joel Takvorian <joel.takvorian@qaraywa.net>

--- vertx-opentracing/src/main/java/examples/OpenTracingExamples.java
+++ vertx-opentracing/src/main/java/examples/OpenTracingExamples.java
@@ -1,10 +1,12 @@
 package examples;
 
+import io.opentracing.Span;
 import io.opentracing.Tracer;
 import io.vertx.core.Vertx;
 import io.vertx.core.VertxOptions;
 import io.vertx.docgen.Source;
 import io.vertx.tracing.opentracing.OpenTracingOptions;
+import io.vertx.tracing.opentracing.OpenTracingUtil;
 
 @Source
 public class OpenTracingExamples {
@@ -23,5 +25,14 @@ public class OpenTracingExamples {
         new OpenTracingOptions(tracer)
       )
     );
+  }
+
+  public void ex3(Tracer tracer) {
+    Span span = tracer.buildSpan("my-operation")
+      .withTag("some-key", "some-value")
+      .start();
+    OpenTracingUtil.setSpan(span);
+    // Do something, e.g. client request
+    span.finish();
   }
 }
