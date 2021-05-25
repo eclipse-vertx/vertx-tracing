dd91c180e6750135f77743c94dc5399cd90809d8
slinkydeveloper <francescoguard@gmail.com>
2021-05-25 12:24:49 +0200

    Some logging for test purpose
    Signed-off-by: Francesco Guardiani <francescoguard@gmail.com>

--- vertx-opentelemetry/src/main/java/io/vertx/tracing/opentelemetry/OpenTelemetryTracer.java
+++ vertx-opentelemetry/src/main/java/io/vertx/tracing/opentelemetry/OpenTelemetryTracer.java
@@ -18,6 +18,8 @@ import io.opentelemetry.context.propagation.ContextPropagators;
 import io.opentelemetry.context.propagation.TextMapGetter;
 import io.opentelemetry.context.propagation.TextMapSetter;
 import io.vertx.core.Context;
+import io.vertx.core.impl.logging.Logger;
+import io.vertx.core.impl.logging.LoggerFactory;
 import io.vertx.core.spi.tracing.SpanKind;
 import io.vertx.core.spi.tracing.TagExtractor;
 import io.vertx.core.spi.tracing.VertxTracer;
@@ -32,6 +34,8 @@ class OpenTelemetryTracer implements VertxTracer<Scope, Scope> {
 
   private static final TextMapGetter<Iterable<Entry<String, String>>> getter = new HeadersPropagatorGetter();
   private static final TextMapSetter<BiConsumer<String, String>> setter = new HeadersPropagatorSetter();
+
+  private static final Logger logger = LoggerFactory.getLogger(OpenTelemetryTracer.class);
 
   private final Tracer tracer;
   private final ContextPropagators propagators;
@@ -52,6 +56,7 @@ class OpenTelemetryTracer implements VertxTracer<Scope, Scope> {
     final TagExtractor<R> tagExtractor) {
 
     if (TracingPolicy.IGNORE.equals(policy)) {
+      logger.debug("Tracing policy ignore");
       return null;
     }
 
@@ -64,6 +69,7 @@ class OpenTelemetryTracer implements VertxTracer<Scope, Scope> {
     // OpenTelemetry SDK's Context is immutable, therefore if the extracted context is the same as the parent context
     // there is no tracing data to propagate downstream and we can return null.
     if (tracingContext == parentContext && TracingPolicy.PROPAGATE.equals(policy)) {
+      logger.debug("The policy is propagate but there is no context to propagate");
       return null;
     }
 
@@ -74,6 +80,10 @@ class OpenTelemetryTracer implements VertxTracer<Scope, Scope> {
       .startSpan();
 
     tagExtractor.extractTo(request, span::setAttribute);
+
+    if (logger.isDebugEnabled()) {
+      logger.debug("Created new receiveRequest span: " + span);
+    }
 
     return VertxContextStorageProvider.VertxContextStorage.INSTANCE.attach(context, tracingContext.with(span));
   }
@@ -87,10 +97,15 @@ class OpenTelemetryTracer implements VertxTracer<Scope, Scope> {
     final TagExtractor<R> tagExtractor) {
 
     if (scope == null) {
+      logger.debug("There is no scope to close");
       return;
     }
 
     Span span = Span.fromContext(context.getLocal(ACTIVE_CONTEXT));
+
+    if (logger.isDebugEnabled()) {
+      logger.debug("Closing span: " + span);
+    }
 
     if (failure != null) {
       span.recordException(failure);
@@ -115,14 +130,14 @@ class OpenTelemetryTracer implements VertxTracer<Scope, Scope> {
     final TagExtractor<R> tagExtractor) {
 
     if (TracingPolicy.IGNORE.equals(policy) || request == null) {
+      logger.debug("Tracing policy ignore");
       return null;
     }
-
-    final io.opentelemetry.api.trace.SpanKind spanKind = SpanKind.RPC.equals(kind) ? io.opentelemetry.api.trace.SpanKind.CLIENT : io.opentelemetry.api.trace.SpanKind.PRODUCER;
 
     io.opentelemetry.context.Context tracingContext = context.getLocal(ACTIVE_CONTEXT);
 
     if (tracingContext == null && !TracingPolicy.ALWAYS.equals(policy)) {
+      logger.debug("No tracing context available and policy is not always");
       return null;
     }
 
@@ -132,10 +147,13 @@ class OpenTelemetryTracer implements VertxTracer<Scope, Scope> {
 
     final Span span = tracer.spanBuilder(operation)
       .setParent(tracingContext)
-      .setSpanKind(spanKind)
+      .setSpanKind(SpanKind.RPC.equals(kind) ? io.opentelemetry.api.trace.SpanKind.CLIENT : io.opentelemetry.api.trace.SpanKind.PRODUCER)
       .startSpan();
-
     tagExtractor.extractTo(request, span::setAttribute);
+
+    if (logger.isDebugEnabled()) {
+      logger.debug("Created new sendRequest span: " + span);
+    }
 
     tracingContext = tracingContext.with(span);
     propagators.getTextMapPropagator().inject(tracingContext, headers, setter);
@@ -150,23 +168,7 @@ class OpenTelemetryTracer implements VertxTracer<Scope, Scope> {
     final Scope scope,
     final Throwable failure,
     final TagExtractor<R> tagExtractor) {
-
-    if (scope == null) {
-      return;
-    }
-
-    Span span = Span.fromContext(context.getLocal(ACTIVE_CONTEXT));
-
-    if (failure != null) {
-      span.recordException(failure);
-    }
-
-    if (response != null) {
-      tagExtractor.extractTo(response, span::setAttribute);
-    }
-
-    span.end();
-    scope.close();
+    this.sendResponse(context, response, scope, failure, tagExtractor);
   }
 
 }
