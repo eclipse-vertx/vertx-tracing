973b0a67585bfa808a8b5ff98b70b0e9651f7de2
Thomas Segismont <tsegismont@gmail.com>
2023-12-12 11:51:08 +0100

    OpenTelemetry: fallback to default storage when not on a Vert.x thread
    The VertxContextStorageProvider is chosen by OpenTelemetry via the service loader mechanism.
    
    While it is mandatory to use context for storage of spans created on Vert.x threads, there may be parts of the application implemented without Vert.x.
    
    In this case, the storage provider fails with NPE. Instead, it is better to fall back to the default storage mechanism, which uses thread local variables.
    
    Signed-off-by: Thomas Segismont <tsegismont@gmail.com>

---
+++ vertx-opentelemetry/src/main/java/io/vertx/tracing/opentelemetry/Operation.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2011-2023 Contributors to the Eclipse Foundation
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0, or the Apache License, Version 2.0
+ * which is available at https://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR Apache-2.0
+ */
+
+package io.vertx.tracing.opentelemetry;
+
+import io.opentelemetry.api.trace.Span;
+import io.opentelemetry.context.Scope;
+
+import java.util.Objects;
+
+final class Operation {
+
+  private final Span span;
+  private final Scope scope;
+
+  Operation(Span span, Scope scope) {
+    this.span = Objects.requireNonNull(span);
+    this.scope = Objects.requireNonNull(scope);
+  }
+
+  public Span span() {
+    return span;
+  }
+
+  public Scope scope() {
+    return scope;
+  }
+}
